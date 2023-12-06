6b7842651e86f7f36ec74b9402fa0d6a96890507
Trend20 <enockomondi305@gmail.com>
2023-12-07 00:26:11 +0300

    adding project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { RatingModule } from './rating/rating.module';
+import { ShippingModule } from './shipping/shipping.module';
+import { RefundModule } from './refund/refund.module';
+import { AddressModule } from './address/address.module';
+import { WhishlistModule } from './whishlist/whishlist.module';
+import { CouponModule } from './coupon/coupon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    CartModule,
+    UserModule,
+    ProductModule,
+    CategoryModule,
+    OrderModule,
+    PaymentModule,
+    RatingModule,
+    ShippingModule,
+    RefundModule,
+    AddressModule,
+    WhishlistModule,
+    CouponModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
